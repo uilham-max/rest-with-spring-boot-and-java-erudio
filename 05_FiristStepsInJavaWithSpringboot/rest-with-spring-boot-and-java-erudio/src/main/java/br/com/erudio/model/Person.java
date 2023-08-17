30f2f70feb63400b2418f1de91c36a95d01f4c52
uilham-max <uilhamgoncalves@gmail.com>
2023-08-16 23:49:46 -0300

    Ajuste no conector mysql no arquivo pom 2

--- 05_FiristStepsInJavaWithSpringboot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/model/Person.java
+++ 05_FiristStepsInJavaWithSpringboot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/model/Person.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
@@ -11,7 +12,7 @@ import jakarta.persistence.Table;
 public class Person implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+	@Id
 	private Long id;
 	private String firstName;
 	private String secondName;
